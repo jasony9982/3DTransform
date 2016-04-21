60994db7f9e6012407b55958cedcfdfa5123bf97
jason <363229427@qq.com>
2016-04-21 18:11:52 +0800

    图片的3D浏览效果

---
+++ 3DTransform/3DTransform/UIView+Extension.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  3DTransform
+//
+//  Created by jason on 15/4/21.
+//  Copyright © 2015年 jason. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Extension)
+
+@property (nonatomic,assign) CGFloat x;
+@property (nonatomic,assign) CGFloat y;
+@property (nonatomic,assign) CGFloat centerX;
+@property (nonatomic,assign) CGFloat centerY;
+@property (nonatomic,assign) CGFloat width;
+@property (nonatomic,assign) CGFloat height;
+@property (nonatomic,assign) CGSize  size;
+
+
+@end
